9120e960a6a7d6645e3634cbae4b5e99be20b9c3
Shawn Lawyer <shawnlawyer@gmail.com>
2020-03-09 11:27:08 -0400

    remove .bash_history

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 docker-compose up -d --build
 
+
+docker exec -it tf_idf_tf_idf_1 bash
+
+hit the up arrow to revisit bash history and find 'jupyter notebook list' and hit enter.
+
+example output: http://0.0.0.0:8888/?token=fd54a13bceabddbda89ce0e66e31989a1b54f8eb9e650339 :: /notebooks
+
+browse to http://localhost:8888/?token=<token_value>
+
+This will bring you the jupyter notebook development area.
+
 docker exec -it tf_idf_jupyter_1 bash
 
 hit the up arrow to revisit bash history and find 'jupyter notebook list' and hit enter.
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from flask import Flask, redirect, render_template, request, session, url_for
 from envs import env
 from flask_wtf import FlaskForm
@@ -6,52 +7,53 @@ from wtforms import SubmitField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from werkzeug.utils import secure_filename
 
+
 #custom modules
 from lib import TF_IDF
-
-ALLOWED_EXTENSIONS = set(['txt'])
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
 app.config['UPLOAD_PATH'] = os.getcwd() + '/data/text'
-
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+app.config['UPLOAD_ALLOWED_EXTENSIONS'] = ['txt']
 
 @app.route('/')
 def home():
-	return render_template('portal.html')
+    return render_template('portal.html')
 
 @app.route('/upload', methods=['GET'])
 def upload_form():
-	return render_template('upload.html')
+    return render_template('upload.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
-		if 'files[]' not in request.files:
-			return redirect(request.url)
-		files = request.files.getlist('files[]')
-		for file in files:
-			if file and allowed_file(file.filename):
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-		return redirect('/results')
+    shutil.rmtree(app.config['UPLOAD_PATH'])
+    os.mkdir(app.config['UPLOAD_PATH'])
+    if request.method == 'POST':
+        if 'files[]' not in request.files:
+            return redirect(request.url)
+        files = request.files.getlist('files[]')
+        for file in files:
+            if file and '.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in app.config['UPLOAD_ALLOWED_EXTENSIONS']:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        return redirect('/results')
 
 @app.route('/results')
 def results():
-    nl =""" """
+
     configs = {
-        "corpus_base_file_path" : "data/text/",
+        "corpus_base_file_path" : app.config['UPLOAD_PATH'],
         "corpus_file_extenions" : ('.txt')
     }
-
+    
     tf_idf = TF_IDF(**configs)
     tf_idf.run()
 
     dataframe = tf_idf.term_frequency_inverse_document_frequency_df
+
     html = dataframe.sort_values(by='weight', ascending=False).head(30).to_html()
+
     return html
 
 if __name__ == '__main__':
